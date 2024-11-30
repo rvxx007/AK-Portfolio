049c99185c9faf04fb34cf12824d6bbc9f5b2cfe
rvxx007 <akashkawle0@gmail.com>
2024-11-30 12:22:12 +0530

    Layout Components
    Layout Finished And All Components Are Created

--- AK-v2.0.0/src/pages/NotFound.tsx
+++ AK-v2.0.0/src/pages/NotFound.tsx
@@ -1,8 +1,15 @@
-
+import notFound from '../assets/404.gif';
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <>
+        <main className=" w-dvw h-dvh flex justify-center items-center text-center">
+            <div className='flex justify-center items-end'>
+                <img src={notFound} alt="404" />
+                <h1 className='font-mono text-2xl font-bold mb-8'>404 Page Not Found</h1>
+            </div>
+        </main>
+    </>
   )
 }
 
