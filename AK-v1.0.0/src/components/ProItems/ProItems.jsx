25812dd8dd61a61c88eafb33ccc283e298df2b22
rvxx007 <akashkawle0@gmail.com>
2024-12-04 18:15:53 +0530

    minor patch

--- AK-v1.0.0/src/components/ProItems/ProItems.jsx
+++ AK-v1.0.0/src/components/ProItems/ProItems.jsx
@@ -5,21 +5,21 @@ const ProItems = (prop) => {
   return (
     <>
     {item.proType===proType &&
-      <motion.div whileHover={{scale:1.1}} className="xl:w-[300px] group-[dd] hover:border-primaryDark w-full bg-bgDark p-0 rounded-md shadow-xl gap-2 border-2 border-gray-800 font-bold flex flex-col justify-between items-center">
+      <motion.div whileHover={{scale:1.1}} className="xl:w-[300px] group-[dd]  w-full bg-bgDark p-0 rounded-md shadow-xl gap-2 border-2 hover:border-primaryDark border-gray-800 font-bold flex flex-col justify-between items-center">
 
       <div className="flex flex-row xl:flex-col">
       <img className="w-[300px] h-[300px]" 
-      src={item.src} alt={item.title} />
+      src={import.meta.env.VITE_BE_BASE_URL+item.src} alt={item.title} />
       <div className="w-full flex p-2 text-wrap flex-col justify-center items-center">
       <h1 className="text-2xl">{item.title}</h1>
       {/* <p className=" font-semibold">{item.desc}</p> */}
       </div>
       </div>
       <div className=" w-full bg-secondaryDark p-2 gap-2 flex flex-row justify-between items-center">
-      <a href={item.gitlink} target="_blank" className="w-full text-center cursor-pointer rounded-md shadow-md text-xl py-2 px-4 bg-bgDark ">
+      <a href={item.gitlink} target="_blank" className="w-full text-center cursor-pointer border-2 hover:border-primaryDark border-slate-950 rounded-md shadow-md text-xl py-2 px-4 bg-bgDark ">
           GitHub
       </a>
-      <a href={item.livelink} target="_blank" className="w-full text-center cursor-pointer rounded-md shadow-md text-xl py-2 px-4 bg-bgDark ">
+      <a href={item.livelink} target="_blank" className="w-full text-center cursor-pointer border-2 hover:border-primaryDark border-slate-950 rounded-md shadow-md text-xl py-2 px-4 bg-bgDark ">
           View
       </a>
       </div>
