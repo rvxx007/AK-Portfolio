741557f7f3d4eca42a227e2630b9a8de28b4f868
rvxx007 <akashkawle0@gmail.com>
2024-12-17 14:37:11 +0530

    resouces added

--- server/app.js
+++ server/app.js
@@ -2,18 +2,24 @@ import express from 'express';
 const app = express();
 import dotenv from 'dotenv';
 dotenv.config({path:".env"});
-
+import path, { dirname } from 'path'
 import mdb from './root/config/mdb.js';
 import akRouter from './root/routes/akRoute.js';
 
 import cors from 'cors';
 import multer from 'multer';
 import adminRouter from './root/routes/adminRoute.js';
+import { fileURLToPath } from 'url';
 const PORT =  process.env.PORT || 2222 ;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('./public'));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+// Set the views directory using path.join
+app.set('views', path.resolve("/root/views")); 
 // Set EJS as templating engine
 app.set("view engine", "ejs")
 
@@ -63,7 +69,8 @@ app.post('/upload',upload.single('image'),async(req, res)=>{
         const data = {
             name:"Akash Kawale",
             desc:"This is a Backend Server for my Portfolio and it's personal server thats why its not available for everyone , please stay away dont try to access endpoints and try to get data.",
-            title:"Portfolio Backend Server"
+            title:"Portfolio Backend Server",
+            imgUrl:"https://ak-portfolio-server.onrender.com/upload/logo.png"
             }
         res.render('/home/rv/Desktop/Workplace/Private/Portfolio/AK-Portfolio/server/root/views/index',{ data});
 
